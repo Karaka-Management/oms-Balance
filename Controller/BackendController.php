55a94fb7955d125ea37078db7880e1714c0e0350
Dennis Eichhorn <spl1nes.com@googlemail.com>
2023-05-30 15:44:14 +0200

    Continue with getter/setter cleanup

--- Controller/BackendController.php
+++ Controller/BackendController.php
@@ -45,7 +45,7 @@ final class BackendController extends Controller
     {
         $view = new View($this->app->l11nManager, $request, $response);
         $view->setTemplate('/Modules/Balance/Theme/Backend/balance-dashboard');
-        $view->addData('nav', $this->app->moduleManager->get('Navigation')->createNavigationMid(1006501001, $request, $response));
+        $view->data['nav'] = $this->app->moduleManager->get('Navigation')->createNavigationMid(1006501001, $request, $response);
 
         return $view;
     }
