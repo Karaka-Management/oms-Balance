de4ecb4d7a9dec08b971a1eb234acda1745971b9
Dennis Eichhorn <spl1nnes.com@googlemail.com>
2023-09-25 22:54:38 +0000

    fix tests

--- tests/Admin/AdminTest.php
+++ tests/Admin/AdminTest.php
@@ -21,7 +21,7 @@ final class AdminTest extends \PHPUnit\Framework\TestCase
 {
     protected const NAME = 'Balance';
 
-    protected const URI_LOAD = '';
+    protected const URI_LOAD = 'http://127.0.0.1/en/backend/controlling/balance';
 
     use \tests\Modules\ModuleTestTrait;
 }
