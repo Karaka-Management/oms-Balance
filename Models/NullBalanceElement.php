b3a8bf3ee39a4cf715150667eb7769bc7694c509
Dennis Eichhorn <spl1nes.com@googlemail.com>
2024-05-21 00:09:06 +0200

    fix version and bugs

--- Models/NullBalanceElement.php
+++ Models/NullBalanceElement.php
@@ -6,7 +6,7 @@
  *
  * @package   Modules\Balance\Models
  * @copyright Dennis Eichhorn
- * @license   OMS License 2.0
+ * @license   OMS License 2.2
  * @version   1.0.0
  * @link      https://jingga.app
  */
@@ -18,7 +18,7 @@ namespace Modules\Balance\Models;
  * Null model
  *
  * @package Modules\Balance\Models
- * @license OMS License 2.0
+ * @license OMS License 2.2
  * @link    https://jingga.app
  * @since   1.0.0
  */
